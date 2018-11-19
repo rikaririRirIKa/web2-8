607f774ab181c7d62f203c7eff228d461d611664
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-19 15:40:16 +0900

    Update default.js

--- default.js
+++ default.js
@@ -13,3 +13,14 @@ while(0 < HP){
   li.textContent = damage + 'のダメージを受けた！残りのHP:' + HP;
   document.getElementById('while').appendChild(li);
 }
+
+var include_tax = function(price){
+  return Math.floor(price + 0.08 * price);
+}
+
+for(var i = 1; i < 10; i++){
+  var li = document.createElement('li');
+  var price = Math.floor(Math.random() * 1000);
+  li.textContent = price + '円(税込' + include_tax(price) + '円)';
+  document.getElementById('price').appendChild(li);
+}
