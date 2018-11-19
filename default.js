05cc9892aec1f03d9e0e2aeb9827072d897c0c2a
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-19 16:32:40 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,19 +1,24 @@
 var separate_time = function(time){
-  var sec   = time.getSeconds();
-  var min   = time.getMinutes();
-  var hours = time.getHours();
-  var days  = time.getDate();
-  var month = time.getMonth();
-  var year  = time.getFullYear();
-  return [sec, min, hours, days, month, year];
+  var sec   = Math.floor(( time / 1000) % 60 );
+  var min   = Math.floor(( time / 1000 / 60) % 60);
+  var hours = Math.floor(( time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor(  time / 1000 / 60 / 60 / 24);
+  return [sec, min, hours, days];
 }
 
-var now = new Date();
+var update = function(){
+  var now = new Date();
+  var target = new Date();
+  var diff = target.getTime() - now.getTime();
+  var counter = separate_time(diff);
 var counter = separate_time(now);
 document.getElementById('countdown').textContent = 
-  counter[5] + '年' +
-  counter[4] + '月' +
+  'オリンピックまであと　' +
   counter[3] + '日' +
-  counter[2] + '時' +
+  counter[2] + '時間' +
   counter[1] + '分' +
-  counter[0] + '秒';
+  counter[0] + '秒' ;
+  refresh();//タイマーを起動
+}
+
+ 
