2c096d8cd94b3d55103f853bd77ff164f4e9b909
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-19 15:54:50 +0900

    Update default.js

--- default.js
+++ default.js
@@ -24,3 +24,10 @@ for(var i = 1; i < 10; i++){
   li.textContent = price + '円(税込' + include_tax(price) + '円)';
   document.getElementById('price').appendChild(li);
 }
+
+var constellatio = ['牡羊座','牡牛座','双子座','蟹座','獅子座','乙女座','天秤座','蠍座','射手座','山羊座','水瓶座','魚座'];
+for(var i = 0; i < 12; i++){
+  var li = docment.creatElement('li');
+  li.textContent = constellatio[i];
+  document.getElementById('array').appendChild(li);
+}
