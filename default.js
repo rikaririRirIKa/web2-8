892f210db693cf2fe89196c49e8c1bb4dad9425b
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-19 17:37:38 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,4 +1,72 @@
-var separate_time = function(time){
+function setCookie(c_name,value,expiredays){
+  //有効期限の日付
+  var extime = new Date().getTime;
+  var cltime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = cltime.toUTCString();
+  //クッキーに保存する文字列を生成
+  var s="";
+  s += c_name +"="+ escape(value);//値はエンコードしておく
+  s +="; path="+ location.pathname;
+  if(expiredays){
+     s +="; expires=" +exdate+";";
+  }else{
+    s +=";";
+  }
+  //クッキーに保存
+  document.cookie=s;
+}
+
+//クッキーの値を取得
+function getCookie(c_name){
+  //有効期限の日付
+  var st = "";
+  var ed = "";
+  if(0 < document.cookie.length){
+    //クッキーの値を取り出す
+    st=document.cookie.indexOf(c_name + "=");
+  if(st!=-1){
+    st=st+c_name.length+1;
+    ed=document.cookie.indexOf( ";",st);
+    if(ed==-1){
+      st=st+c_name.length+1;
+    //値をデコードして返す
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+    return "";
+}
+  
+  //クッキーを読み込んで表示する文章を変える
+  var last_date = getCookie('lasDate');
+  if(last_date){
+    document.getElementById('cookie').textContent = '前回訪れた時間：' + last_date;
+  }else{
+    document.getElementById('cookie').textContent = 'はじめまして';
+  }
+  
+  //新しい値の保存
+  var current_time = new Date();
+  setCookie('lastDate', current_time.toString(),7);
+  
+{function getFileName(){
+  //ホスト以下のパスを取得し、それをsplit('/')で「/」区切りで配列に入れ、pop()で配列の最後の値を取得
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.select = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
+
+  var separate_time = function(time){
   var sec   = Math.floor(( time / 1000) % 60 );
   var min   = Math.floor(( time / 1000 / 60) % 60);
   var hours = Math.floor(( time / 1000 / 60 / 60) % 24);
