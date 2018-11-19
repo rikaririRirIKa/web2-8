798ae393c18e429eff888c20552168289098584b
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-19 16:04:58 +0900

    Update default.js

--- default.js
+++ default.js
@@ -31,3 +31,10 @@ for(var i = 0; i < 12; i++){
   li.textContent = constellatio[i];
   document.getElementById('array').appendChild(li);
 }
+
+var player = {'ぽんぽん', age:17, blood:'AB'};
+for(key in player){
+  var li = document.createElement('li');
+  li.textContent - key + ':' + player[key];
+  document.getElementById('object').appendChild(li);
+}
